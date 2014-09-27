bfabf8db531ad8364341f94673edfce623685735
edmundwong <elementwong@sohu.com>
2014-09-27 22:46:43 +0800

    更新DZ3.1补丁

--- data/sysdata/cache_mobile.php
+++ data/sysdata/cache_mobile.php
@@ -1,6 +1,6 @@
 <?php
 //Discuz! cache file, DO NOT modify me!
-//Identify: b8b6a7887a8f40362e85faa9e57a683e
+//Identify: e9c28e69e897e86ecb81811b2cfc7e15
 
-$mobilecheck = '{"discuzversion":"X3.1","charset":"utf-8","version":"3","pluginversion":"1.3.1","oemversion":"0","regname":"register","qqconnect":"0","sitename":"\\u5927\\u6e29\\u540c\\u57ce80","mysiteid":"","ucenterurl":"http:\\/\\/localhost\\/uc_server","setting":{"closeforumorderby":null},"extends":{"used":null,"lastupdate":null}}';
+$mobilecheck = '{"discuzversion":"X3.1","charset":"utf-8","version":"3","pluginversion":"1.3.1","oemversion":"0","regname":"register","qqconnect":"0","sitename":"\\u5927\\u6e29\\u540c\\u57ce","mysiteid":"","ucenterurl":"http:\\/\\/localhost\\/uc_server","setting":{"closeforumorderby":null},"extends":{"used":null,"lastupdate":null}}';
 ?>
--- data/template/11_sale_default_style.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php if(!defined('IN_DISCUZ')) exit('Access Denied'); ?>
-<script language="javascript" >function killErr(){return true;}var root = "<?php echo $sale_config['root'];?>";</script>
-<script src="<?php echo $sale_config['sale'];?>static/js/common.js?<?php echo VERHASH;?>" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="<?php echo $sale_config['sale'];?>template/<?php echo $style;?>/css/style.css?<?php echo VERHASH;?>" />
-<?php if($sale_config['header']) { ?><style>#hd{display:none}</style><?php } if($sale_config['scbar']) { ?><style>#scbar {display:none} </style>  <?php } if($sale_config['diy_css']) { ?><style type="text/css"><?php echo $sale_config['diy_css'];?></style><?php } if(empty($_GET['diy'])) { ?><style type="text/css">.frame, .frame-tab { background-color:inherit;  border:none; margin:0;}.block { margin:0;}</style><?php echo $_lang['discuz_diy'];?><?php } ?>
-<style id="diy_style" type="text/css"></style>
