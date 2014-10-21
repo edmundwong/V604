550901149f78346b5636b1ca0211d23a960d3157
edmundwong <elementwong@sohu.com>
2014-10-21 22:33:12 +0800

    sale spider config

--- source/plugin/sale/module/spider/config.php
+++ source/plugin/sale/module/spider/config.php
@@ -8,16 +8,77 @@ $__links = array(
         'title' => '二手物品/求购',
         'link' => 'http://www.vanpeople.com/c/list/4.html',
         'child' => array(
-            2 => array(
-                    'title' => '汽车及配件 (私人)',
-                    'link' => 'http://www.vanpeople.com/c/list/49/0/0.html'
-                    //http://www.vanpeople.com/c/list/49/0/60.html
-                    //http://www.vanpeople.com/c/list/49/0/120.html
-                ),
-            3 => array(
-                    'title' => '汽车及配件 (车行)',
-                    'link' => 'http://www.vanpeople.com/c/list/183.html'
-                )
+            2 => array('link' => 'http://www.vanpeople.com/c/list/49/0/0.html','title' => '汽车及配件 (私人)'),
+            3 => array('link' => 'http://www.vanpeople.com/c/list/183/0/0.html','title' => '汽车及配件 (车行)'),
+            46 => array('link' => 'http://www.vanpeople.com/c/list/187/0/0.html','title' => '自行车/摩托车/游艇'),
+            4 => array('link' => 'http://www.vanpeople.com/c/list/50/0/0.html','title' => '电脑及配件'),
+            6 => array('link' => 'http://www.vanpeople.com/c/list/188/0/0.html','title' => '数码/平板'),
+            5 => array('link' => 'http://www.vanpeople.com/c/list/51/0/0.html','title' => '手机通讯'),
+            47 => array('link' => 'http://www.vanpeople.com/c/list/189/0/0.html','title' => '游戏装备'),
+            7 => array('link' => 'http://www.vanpeople.com/c/list/52/0/0.html','title' => '二手家具/家居日用'),
+            48 => array('link' => 'http://www.vanpeople.com/c/list/192/0/0.html','title' => '办公/设备'),
+            49 => array('link' => 'http://www.vanpeople.com/c/list/193/0/0.html','title' => '建材/工具'),
+            50 => array('link' => 'http://www.vanpeople.com/c/list/115/0/0.html','title' => '家用电器/日用品'),
+            8 => array('link' => 'http://www.vanpeople.com/c/list/116/0/0.html','title' => '书本/音像'),
+            13 => array('link' => 'http://www.vanpeople.com/c/list/117/0/0.html','title' => '月票/门票/折扣礼品卡'),
+            51 => array('link' => 'http://www.vanpeople.com/c/list/190/0/0.html','title' => '乐器'),
+            52 => array('link' => 'http://www.vanpeople.com/c/list/191/0/0.html','title' => '艺术收藏'),
+            9 => array('link' => 'http://www.vanpeople.com/c/list/104/0/0.html','title' => '服饰箱包/鞋/化妆品'),
+            10 => array('link' => 'http://www.vanpeople.com/c/list/182/0/0.html','title' => '休闲运动'),
+            12 => array('link' => 'http://www.vanpeople.com/c/list/105/0/0.html','title' => '母婴/儿童/玩具'),
+            11 => array('link' => 'http://www.vanpeople.com/c/list/163/0/0.html','title' => '宠物/用品'),
+            53 => array('link' => 'http://www.vanpeople.com/c/list/186/0/0.html','title' => '食品/海农产品'),
+            14 => array('link' => 'http://www.vanpeople.com/c/list/58/0/0.html','title' => '其他物品（包括烟酒）'),
+            15 => array('link' => 'http://www.vanpeople.com/c/list/98/0/0.html','title' => '求购/免费')
+        )
+    ),
+    16 => array(
+        'title' => '房屋信息',
+        'link' => 'http://www.vanpeople.com/c/list/1.html',
+        'child' => array(
+            17 => array('link' => 'http://www.vanpeople.com/c/list/42/0/0.html','title' => '房屋出租/合租'),
+            18 => array('link' => 'http://www.vanpeople.com/c/list/48/0/0.html','title' => '房屋求租'),
+            54 => array('link' => 'http://www.vanpeople.com/c/list/45/0/0.html','title' => '办公室/商铺出租'),
+            55 => array('link' => 'http://www.vanpeople.com/c/list/194/0/0.html','title' => '车位仓库'),
+            19 => array('link' => 'http://www.vanpeople.com/c/list/44/0/0.html','title' => '地产出售'),
+            56 => array('link' => 'http://www.vanpeople.com/c/list/195/0/0.html','title' => '地产求购'),
+            20 => array('link' => 'http://www.vanpeople.com/c/list/196/0/0.html','title' => '生意转让/投资')
+                                            
+        )
+    ),
+    21 => array(
+        'title' => '求职/招聘',
+        'link' => 'http://www.vanpeople.com/c/list/60.html',
+        'child' => array(
+            22 => array('link' => 'http://www.vanpeople.com/c/list/65/0/0.html','title' => '餐饮行业'),
+            23 => array('link' => 'http://www.vanpeople.com/c/list/66/0/0.html','title' => '保姆看护'),
+            24 => array('link' => 'http://www.vanpeople.com/c/list/121/0/0.html','title' => '保洁清洁'),
+            25 => array('link' => 'http://www.vanpeople.com/c/list/67/0/0.html','title' => '销售客服'),
+            26 => array('link' => 'http://www.vanpeople.com/c/list/122/0/0.html','title' => '美容护理'),
+            27 => array('link' => 'http://www.vanpeople.com/c/list/123/0/0.html','title' => '各类工人'),
+            28 => array('link' => 'http://www.vanpeople.com/c/list/124/0/0.html','title' => '司机'),
+            29 => array('link' => 'http://www.vanpeople.com/c/list/68/0/0.html','title' => '电脑相关'),
+            30 => array('link' => 'http://www.vanpeople.com/c/list/69/0/0.html','title' => '教育培训'),
+            31 => array('link' => 'http://www.vanpeople.com/c/list/94/0/0.html','title' => '文秘助理'),
+            32 => array('link' => 'http://www.vanpeople.com/c/list/125/0/0.html','title' => '医疗护理'),
+            33 => array('link' => 'http://www.vanpeople.com/c/list/126/0/0.html','title' => '金融财会'),
+            34 => array('link' => 'http://www.vanpeople.com/c/list/70/0/0.html','title' => '其他工作'),
+            '' => array('link' => 'http://www.vanpeople.com/c/list/197/0/0.html','title' => '义工/志愿者'),
+            35 => array('link' => 'http://www.vanpeople.com/c/list/99/0/0.html','title' => '求职')
+        )
+    ),
+    36 => array(
+        'title' => '其他信息',
+        'link' => 'http://www.vanpeople.com/c/list/100.html',
+        'child' => array(
+            38 => array('link' => 'http://www.vanpeople.com/c/list/109/0/0.html','title' => '回国带物'),
+            37 => array('link' => 'http://www.vanpeople.com/c/list/103/0/0.html','title' => '外币兑换'),
+            39 => array('link' => 'http://www.vanpeople.com/c/list/155/0/0.html','title' => '找人拼车/搭车'),
+            40 => array('link' => 'http://www.vanpeople.com/c/list/102/0/0.html','title' => '寻人寻物'),
+            41 => array('link' => 'http://www.vanpeople.com/c/list/154/0/0.html','title' => '寻求服务'),
+            42 => array('link' => 'http://www.vanpeople.com/c/list/184/0/0.html','title' => '交友征婚'),
+            44 => array('link' => 'http://www.vanpeople.com/c/list/101/0/0.html','title' => '其他个人广告'),
+            43 => array('link' => 'http://www.vanpeople.com/c/list/185/0/0.html','title' => '结伴出游')
         )
     )
 );
